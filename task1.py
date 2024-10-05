0d0de9d9700e685764b3f855850ba6edb14790cb
DaviejayS <victorinocapito@gmail>
2024-10-05 11:01:50 +0800

    kapoya uy

---
+++ task1.py
@@ -0,0 +1,19 @@
+number = int(input("Enter number"))
+num = []
+
+for data in range(number):
+    number = int(input("my number: " ))
+    num.append(number)
+    print(num)
+
+for data in num:
+    if data > 0:
+        print(str(data) + "positive")
+    elif data < 0:
+        print(str(data) + "negative")
+                
+    
+
+
+
+    
